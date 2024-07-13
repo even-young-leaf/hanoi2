f4ff3991457fc1fdaa9074db0ecbdcf9af48b0b9
evenyoung <3245151352@qq.com>
2024-07-13 14:03:11 +0800

    test

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//将n个圆盘从柱x移至柱z，可借助y
+void Hanoi(int n,char x,char y,char z);
+
+int main(void)
+{
+    int n;//圆盘数量
+    scanf("%d",&n);
+    Hanoi(n,'A','B','C');
+    return 0;
+}
+
+void Hanoi(int n,char x,char y,char z)
+{
+    if(n==1)
+    {
+        printf("%c->%c\n",x,z);
+    }else
+    {
+        Hanoi(n-1,x,z,y);
+        printf("%c->%c\n",x,z);
+        Hanoi(n-1,y,x,z);
+    }
+}
